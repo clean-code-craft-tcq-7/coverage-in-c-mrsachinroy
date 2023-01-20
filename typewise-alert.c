52c878f6f32af15e84ac69ee977d65240d343ae7
SACHIN ROY <sachinkumarsnp@gmail.com>
2023-01-20 13:51:36 +0530

    Update_01

--- typewise-alert.c
+++ typewise-alert.c
@@ -48,16 +48,12 @@ void sendToController(BreachType breachType) {
 }
 
 void sendToEmail(BreachType breachType) {
-  // const char* recepient = "a.b@c.com";
-  //     printf("To: %s\n", recepient);
-  // switch(breachType) {
-  //   case TOO_LOW:
-  //     printf("Hi, the temperature is too low\n");
-  //     break;
-  //   case TOO_HIGH:
-  //     printf("Hi, the temperature is too high\n");
-  //     break;
-  //   case NORMAL:
-  //     break;
-  // }
+  const char* recepient = "a.b@c.com";
+  printf("To: %s\n", recepient);
+  if(breachType == TOO_LOW) {
+      printf("Hi, the temperature is too low\n");
+  }
+  if(breachType == TOO_HIGH) {
+      printf("Hi, the temperature is too low\n");
+  }
 }
